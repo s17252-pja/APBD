e237745a4b859f863c17bd122f197e7894e25a6f
s17252 <s17252@pjwstk.edu.pl>
2020-03-27 16:13:36 +0100

    Actually implement the ability to switch between XML and JSON modes Also dispose the stream once we're done using it

--- Tutorial2/Cw2/Program.cs
+++ Tutorial2/Cw2/Program.cs
@@ -10,15 +10,17 @@ namespace Cw2
 {
     class Program
     {
+        private static string mode;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
             string path;
+            ;
             FileStream writer;
-            if (args.Length > 0) { path = args[0]; } else { path = @"Data\dane.csv"; };
-
-            if (args[1].Length > 0) { writer = new FileStream(args[1], FileMode.Create); } else { writer = new FileStream(@"data.xml", FileMode.Create); }
-            if (args[2].Length > 0 && args[2].Contains("xml")) { Console.WriteLine("Running XML mode \n"); } else { Console.WriteLine("Invalid output type provided \n"); }
+            if (args.Length > 0) { path = args[0]; } else { path = @"Data\dane.csv"; Console.WriteLine("No file provided as the first argument, using built-in dane.csv \n"); };
+            if (args[1].Length > 0) { writer = new FileStream(args[1], FileMode.Create); } else { writer = new FileStream(@"data.xml", FileMode.Create); Console.WriteLine("No output file argument specified. Using default data.xml"); }
+            if (args[2].Length > 0 && args[2].Contains("xml")) { Console.WriteLine("Running XML mode \n"); mode = "xml"; } else if (args[2].Contains("json")) { Console.WriteLine("Running JSON mode \n"); mode = "json"; } else { Console.WriteLine("Invalid output type provided \n"); }
 
 
             //Wczytywanie 
@@ -28,8 +30,7 @@ namespace Cw2
             string line = null;
             try
             {
-                using (var stream = new StreamReader(fi.OpenRead()))
-              
+                using var stream = new StreamReader(fi.OpenRead());
                 while ((line = stream.ReadLine()) != null)
                 {
                     string[] student = line.Split(',');
@@ -45,15 +46,14 @@ namespace Cw2
                         MothersName = student[7],
                         FathersName = student[8]
                     };
-                        list.Add(fileStudent);
-                    }
-                } catch (FileNotFoundException ex)
+                    list.Add(fileStudent);
+                }
+                stream.Dispose();
+            }
+            catch (FileNotFoundException ex)
             {
                 Console.WriteLine(ex);
-              
             }
-            //stream.Dispose();
-
             //XML
             //var list = new List<Student>();
             //var st = new Student
@@ -70,18 +70,27 @@ namespace Cw2
 
             //list.Add(st);
 
-
-            FileStream writerXML = new FileStream(@"data.xml", FileMode.Create);
             University u = new University { CreatedAt = DateTime.Now, Author = "Jan Kowalski", students = list };
             UniversityWrapper uw = new UniversityWrapper { University = u };
             XmlSerializer serializer = new XmlSerializer(typeof(University));
-            var jsonString = JsonSerializer.Serialize(uw, typeof(UniversityWrapper));
-            File.WriteAllText("data.json", jsonString);
-                                       //new XmlRootAttribute("university"));
-            serializer.Serialize(writer, u);
-            serializer.Serialize(writerXML, u);
-            //serializer.Serialize(writer, list);
 
+            //new XmlRootAttribute("university"));
+            if (mode.Contains("json"))
+            {
+                var jsonString = JsonSerializer.Serialize(uw, typeof(UniversityWrapper));
+                File.WriteAllText("data.json", jsonString);
+                serializer.Serialize(writer, u);
+            }
+            else if (mode.Contains("xml"))
+            {
+                FileStream writerXML = new FileStream(@"data.xml", FileMode.Create);
+                serializer.Serialize(writerXML, u);
+            }
+            else
+            {
+                Console.WriteLine("You did not provide third parameter! Not writing to any format! \n");
+            }
+            //serializer.Serialize(writer, list);
         }
     }
 }
