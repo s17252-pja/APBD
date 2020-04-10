7bff13a07cb92c897e4d24814749142d0ec75eb3
s17252-pja <s17252@pjwstk.edu.pl>
2020-04-10 14:07:49 +0200

    Upload task5 and task6 - not completed

---
+++ Tutorial5/Zajęcia 5/Wyklad5/Wyklad5/Controllers/EnrollmentsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Wyklad5.DTOs.Requests;
+using Wyklad5.DTOs.Responses;
+using Wyklad5.Models;
+using Wyklad5.Services;
+
+namespace Wyklad5.Controllers
+{
+    [Route("api/enrollments")]
+    [ApiController] //-> implicit model validation
+    public class EnrollmentsController : ControllerBase
+    {
+        private IStudentDbService _service;
+
+        public EnrollmentsController(IStudentDbService service)
+        {
+            _service = service;
+        }
+
+
+        [HttpPost]
+        public IActionResult EnrollStudent(EnrollStudentRequest request)
+        {
+            _service.EnrollStudent(request);
+            var response = new EnrollStudentResponse();
+            //response.LastName = st.LastName;
+            //...
+
+            return Ok(response);
+        }
+
+        //..
+
+        //..
+
+
+    }
+}
