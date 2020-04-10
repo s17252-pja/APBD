caa827e15b2a0a0a81957c8890157d1c998e85f0
s17252-pja <s17252@pjwstk.edu.pl>
2020-04-10 14:23:26 +0200

    add notes to 6

---
+++ Tutorial6/WebApplication2/Startup.cs
@@ -0,0 +1,141 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using WebApplication2.Middlewares;
+using WebApplication2.Models;
+using WebApplication2.Services;
+using static System.Net.WebRequestMethods;
+
+namespace WebApplication2
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IDbService, SqlServerDbService>();
+            services.AddControllers(config =>
+            {
+                config.Filters.Add(typeof(CustomExceptionFilter));
+            });
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IDbService dbService)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Use(async (context, next) =>
+            {
+                if (!context.Request.Headers.ContainsKey("Index"))
+                {
+                    context.Response.StatusCode = Microsoft.AspNetCore.Http.StatusCodes.Status401Unauthorized;
+                    await context.Response.WriteAsync("The index was not provided in the header");
+                    return;
+                }
+                else
+                {
+                    //We've got the right index here
+                    //Check if the student is in the DB
+                    string index = context.Request.Headers["Index"].ToString();
+                    // ..
+                    // new SqlConnection()
+                    // dbService.CheckIndex();
+                    // Select * from Student where idStudent = @Index
+                    // 401
+                    // return;
+                }
+
+
+                //the student is in the DB && validated
+                await next();
+            });
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            //Globalna obs³uga b³êdów
+            //app.UseExceptionHandler(options =>
+            //{
+            //    options.Run(
+            //    async context =>
+            //    {
+            //        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            //        context.Response.ContentType = "application/json";
+
+            //        var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+            //        if (contextFeature != null)
+            //        {
+            //            await context.Response.WriteAsync(new ErrorDetails()
+            //            {
+            //                StatusCode = context.Response.StatusCode,
+            //                Message = "Internal Server Error."
+            //            }.ToString());
+            //        }
+            //    });
+            //});
+
+            //app.UseMiddleware<LoggingMiddleware>();
+            app.Use(async (context, next) =>
+            {
+                if (!context.Request.Headers.ContainsKey("Index"))
+                {
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    await context.Response.WriteAsync("Nie poda³eœ indeksu");
+                    return;
+                }
+
+                string index = context.Request.Headers["Index"].ToString();
+                //check in db
+
+
+                await next();
+            });
+            
+
+
+            app.UseRouting();  // /api/students/10/grades GET   -->  StudentsController i GetStudents
+
+            //......
+
+            app.UseEndpoints(endpoints => // Wykonuje zadania GetStudents()
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
